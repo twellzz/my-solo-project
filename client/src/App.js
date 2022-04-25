6e2c3fc1dae52cdf37ff8f33cb4dbb7d7d6f0200
Tyler Wells <tyler.f.wells@gmail.com>
2022-04-25 15:42:22 -0300

    about to rearrange everything and try and make it responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import SideBar from "./components/SideBar/SideBar";
 import TodaysPatients from "./components/TodaysPatients/TodaysPatients";
+import NewAppt from "./components/NewAppt/NewAppt";
+import ApptConsult from "./components/ApptConsult/ApptConsult";
 
 function App() {
   return (
     <div className="container">
       <SideBar />
-      <TodaysPatients />
+      <div class="top-container">
+        <NewAppt />
+        <ApptConsult />
+      </div>
+      {/* <TodaysPatients /> */}
     </div>
   );
 }
---
+++ client/src/components/ApptConsult/ApptConsult.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./style.css";
+
+export default function ApptConsult() {
+  return <div>ApptConsult</div>;
+}
