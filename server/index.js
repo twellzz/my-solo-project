5be45696c044d20d44f7059ce3741bde87227880
Tyler Wells <tyler.f.wells@gmail.com>
2022-04-26 09:31:23 -0300

    server succesfully turned on

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
+"use strict";
+
 const express = require("express"); // require the express server
+const cors = require("cors"); // require cors
 const app = express(); // instantiate the server
 const port = 1234; // define the port number
 const router = require("./router/index.js"); // import the router file from the router folder
+
+app.use(cors); // turn on cors
 
 app.use(express.json()); // turn on the body parser which allows us to receive json messages
 
 app.use(router); // turn on the router
 
-app.listen(port); // turn on the server at the defined port number
+app.listen(port, () =>
+  console.log(`SERVER IS TURNED ON AND WE ARE NOW LISTENING ON PORT # ${port}`)
+); // turn on the server at the defined port number
---
+++ server/router/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const express = require("express"); // require express
+
+const router = express.Router(); // instantiate the router (needs capital R)
+
+module.exports = router;
